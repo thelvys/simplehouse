bb7cced11420e4cf87729a7173551b94eb3b5b72
thely <thelvys80@gmail.com>
2024-09-09 08:41:55 +0200

    initial 2

---
+++ salooninventory/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SalooninventoryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'salooninventory'
---
+++ saloonservices/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SaloonservicesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'saloonservices'
